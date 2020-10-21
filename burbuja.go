07a469cddb1659a755a0db2b895fadaac77266c2
María Cano <maria.cano4435@alumnos.udg.mx>
2020-10-21 14:32:29 -0500

    Prueba A

--- burbuja.go
+++ burbuja.go
@@ -1,7 +1,17 @@
 package main
-
 func Burbuja(s []int64)  {
-
+	var aux int64;
+	var t,j,i int;
+	t=len(s);
+	for  i=1; i<t ;i++{
+		for j=0;j< (t-2);j++{
+			if s[j]> s[j+1]{
+				aux=s[j];
+				s[j]=s[j+1]
+				s[j+1]=aux;
+			}
+		}
+	}
 }
 
 func main()  {

4f603ecf1070ccbca86ee29e67be808914a6d6ad
María Cano <maria.cano4435@alumnos.udg.mx>
2020-10-21 20:51:18 -0500

    Prueba C

--- burbuja.go
+++ burbuja.go
@@ -1,20 +1,20 @@
 package main
-func Burbuja(s []int64) []int64{
-	var aux int64;
-	var t,j,i int;
-	t=len(s);
-	for  i=1; i<t ;i++{
-		for j=0;j< (t-2);j++{
-			if s[j]> s[j+1]{
-				aux=s[j];
-				s[j]=s[j+1]
-				s[j+1]=aux;
+
+func Burbuja(s []int64) []int64 {
+	var aux int64
+	var j, i int
+	for i = 0; i < len(s)-1; i++ {
+		for j = 0; j < len(s)-1; j++ {
+			if s[j] > s[j+1] {
+				aux = s[j]
+				s[j] = s[j+1]
+				s[j+1] = aux
 			}
 		}
 	}
-	return s;
+	return s
 }
 
-func main()  {
-	
+func main() {
+
 }
